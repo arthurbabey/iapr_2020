88a8b905365979cf3acf021c301baf51cda04da8
mattbuot <matthieu2301@hotmail.fr>
2020-05-10 17:41:24 +0200

    added data

--- project/video_to_Images.py
+++ project/video_to_Images.py
@@ -1,12 +1,12 @@
 import cv2
-file_loc=str(input("Please enter your video location"))
+file_loc='data/robot_parcours_1.avi'
 vidcap = cv2.VideoCapture(file_loc)
 print(cv2.__version__)
 count = 0
 success = True
 while success:
     success,image = vidcap.read(1000)
-    cv2.imwrite("frame%d.tiff" % count, image)     # save frame as JPEG file in the same location as the script
+    cv2.imwrite("data/frame%d.tiff" % count, image)     # save frame as JPEG file in the same location as the script
     success,image = vidcap.read()
     print('Read a new frame: ', success)
     count += 1
