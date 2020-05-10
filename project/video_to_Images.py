9871bda4c9b8c62bb8dbd0c6bcc46e1987422e6b
mattbuot <matthieu2301@hotmail.fr>
2020-05-10 17:50:19 +0200

    Removed read

--- project/video_to_Images.py
+++ project/video_to_Images.py
@@ -8,6 +8,6 @@ while success:
     success,image = vidcap.read(500)
     if success:
         cv2.imwrite("data/frame%d.jpeg" % count, image)     # save frame as JPEG file in the same location as the script
-    success,image = vidcap.read()
+    #success,image = vidcap.read()
     print('Read a new frame: ', success)
     count += 1
