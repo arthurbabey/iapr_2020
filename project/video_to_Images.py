279eec898bf3c40e2cac070fd4ed5ccf2fdf65f5
mattbuot <matthieu2301@hotmail.fr>
2020-05-10 17:49:03 +0200

    Removed last empty image

--- project/video_to_Images.py
+++ project/video_to_Images.py
@@ -5,8 +5,9 @@ print(cv2.__version__)
 count = 0
 success = True
 while success:
-    success,image = vidcap.read(1000)
-    cv2.imwrite("data/frame%d.tiff" % count, image)     # save frame as JPEG file in the same location as the script
+    success,image = vidcap.read(500)
+    if success:
+        cv2.imwrite("data/frame%d.jpeg" % count, image)     # save frame as JPEG file in the same location as the script
     success,image = vidcap.read()
     print('Read a new frame: ', success)
     count += 1
