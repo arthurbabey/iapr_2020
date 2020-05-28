6a9cbe455c59a837a7ddb38e5b742ed171b7d567
arthurbabey <arthur.babey@epfl.ch>
2020-05-28 20:26:20 +0200

    final main et modeles

--- project/main.py
+++ project/main.py
@@ -16,7 +16,7 @@ def contours_dist(c1, c2):
 
 # returns the center of a contour
 def center(contour):
-    
+
     M = cv2.moments(contour)
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
@@ -26,7 +26,7 @@ def center(contour):
 def preprocess_img(sub_img):
     size = 28
     sub_img = cv2.cvtColor(sub_img, cv2.COLOR_BGR2GRAY)
-    
+
     sub_img = cv2.resize(sub_img, (size, size), interpolation = cv2.INTER_AREA)
     sub_img = sub_img.astype('float32')
     sub_img /= 255
@@ -46,25 +46,25 @@ def find_robot(img):
     mask = np.array((blues < 100) & (greens < 100) & (reds > 100), dtype=np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    
+
     contours, hierarchy = cv2.findContours(mask,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
     areas = [cv2.contourArea(c) for c in contours]
     max_index = np.argmax(areas)
     contour = contours[max_index]
-    
+
     return center(contour)
 
 def draw_robot_pos(img, pos):
     for p in pos:
-        img = cv2.drawMarker(img, p, (0, 255, 0), thickness=1)
+        img = cv2.drawMarker(img, p, (0, 255, 0), thickness=2)
     return img
 
 def robot_on_char(robot_p, char_centers):
     min_dist = min(cdist([robot_p], char_centers).flatten())
     if min_dist < 30:
         return np.argmin(cdist([robot_p], char_centers).flatten())
-    
+
 
 def clipped_zoom(img, zoom_factor, **kwargs):
 
@@ -111,7 +111,7 @@ def clipped_zoom(img, zoom_factor, **kwargs):
     return out
 
 def main(argv):
-    
+
     """
     Encoding: plus = 0, minus = 1, div = 2, times = 3, egal = 4
     """
@@ -144,16 +144,16 @@ def main(argv):
     characters = []
 
     while len(contours) > 0:
-        
+
         shape = contours.pop(0)
-        
+
         while contours and min([contours_dist(shape, c) for c in contours]) < 20:
             closest = np.argmin([contours_dist(shape, c) for c in contours])
-            shape = np.concatenate([shape, contours.pop(closest)])    
-        
+            shape = np.concatenate([shape, contours.pop(closest)])
+
         if (cv2.contourArea(shape) > 50) and (cv2.contourArea(shape) < 300):
             characters.append(shape)
-            
+
     # Part 2: map to all the characters a prediction for being an operator and a digit
 
     digit_classifier = load_model('digit_classifier.h5')
@@ -167,15 +167,15 @@ def main(argv):
         cX, cY = center(c)
         char_centers.append([cX, cY])
         size = 40
-        
+
         sub_img = frame0[int(cY-size/2):int(cY+size/2), int(cX-size/2):int(cX+size/2)]
         img = preprocess_img(sub_img)
         pred_o = np.argmax(operator_classifier.predict(img))
         pred_d = np.argmax(digit_classifier.predict(img))
-            
+
         index_to_char[i] = {True: str(pred_d), False: pred_to_operator[pred_o]}
 
-        
+
     # Part 3: for each image, test if the robot is stepping on a character and update the equation
 
     out_frames = []
@@ -192,22 +192,22 @@ def main(argv):
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             robot_pos.append(find_robot(img))
             draw_robot_pos(img, robot_pos)
-            
+
             detection = robot_on_char(robot_pos[-1], char_centers)
 
             if (detection is not None) and detection != previous_detected:
-                
+
                 equation += ' ' + index_to_char[detection][is_digit]
-                
+
                 if index_to_char[detection][is_digit] == '=':
                     equation += ' ' + str(eval(equation[:-2]))
 
                 print(equation)
                 is_digit = not is_digit
                 previous_detected = detection
-                
-            img = cv2.putText(img, equation, equation_pos, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
-            
+
+            img = cv2.putText(img, equation, equation_pos, cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), thickness=3)
+
             out_frames.append(img)
 
 
